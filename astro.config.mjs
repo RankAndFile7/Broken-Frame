0c3f92bca8d22130f7aaa16245497607afd514db
RankAndFile7 <robmorris88@gmail.com>
2026-09-18 10:30:49 -0700

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,6 @@
 import { defineConfig } from 'astro/config';
 
-// https://astro.build
 export default defineConfig({
-  // Set this to your production domain before deploying (used for canonical URLs / sitemap).
-  site: 'https://brokenframestrategies.com',
+  site: 'https://rankandfile7.github.io',
+  base: '/Broken-Frame',
 });
